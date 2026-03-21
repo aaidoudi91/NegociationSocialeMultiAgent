1059ba4cb9866fb02d239ae08ec2dfd5399adfb7
Aaron <161750474+aaidoudi91@users.noreply.github.com>
2026-03-21 17:56:57 +0100

    ajout Dimension.java

--- src/main/java/app/model/Dimension.java
+++ src/main/java/app/model/Dimension.java
@@ -1,1 +1,12 @@
 package app.model;
+
+/** Énumération des six dimensions négociables, permet de s'assurer que les deux agents parlent exactement du même sujet
+ * lors d'une argumentation. */
+public enum Dimension {
+    POSTES_SUPPRIMES,
+    DUREE_REQUALIFICATION, // en mois
+    COMPENSATION, // en mois de salaire
+    RYTHME_DEPLOIEMENT, // en mois
+    PRIORITE_RECRUTEMENT, // boolean
+    COMITE_SUIVI // boolean
+}
