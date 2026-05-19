55d2abc4869a07c199eb6c086b44dcb6de277456
Aaron <161750474+aaidoudi91@users.noreply.github.com>
2026-05-19 13:30:55 +0200

    Màj SyndicatAgent.java

--- src/main/java/app/agents/SyndicatAgent.java
+++ src/main/java/app/agents/SyndicatAgent.java
@@ -2,21 +2,28 @@ package app.agents;
 
 import app.behaviours.NegotiationBehaviour;
 import app.knowledge.SyndicatKB;
+import app.raison.RaisonClient;
 import jade.core.Agent;
 import jade.domain.DFService;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.FIPAException;
 
-/** Agent représentant le Syndicat des travailleurs dont l'objectif est de rotéger les emplois et maximiser les
+/** Agent représentant le Syndicat des travailleurs dont l'objectif est de protéger les emplois et maximiser les
  * garanties pour les travailleurs. Il est récepteur de la négociation. */
 public class SyndicatAgent extends Agent {
+    private static final String API_KEY = "";
+    private static final String APP_ID = "PRJ30475"; // Projet RAISON du Syndicat
+
     @Override
     protected void setup() {
         System.out.println("[SYNDICAT] Agent démarré : " + getAID().getName());
         registerToDF(); // S'enregistre pour que la Direction puisse le trouver
-        // Lance le comportement de négociation avec sa propre KB
-        addBehaviour(new NegotiationBehaviour(this, new SyndicatKB(), null, false));
+        RaisonClient raisonClient = new RaisonClient(API_KEY, APP_ID);
+        raisonClient.printMetadata();
+        // Lance le comportement de négociation avec sa propre KB et son instance RAISON
+        // destinataire = null : sera découvert depuis le premier message reçu
+        addBehaviour(new NegotiationBehaviour(this, new SyndicatKB(), null, false, raisonClient));
     }
 
     // Publie le service "negociation" dans le Directory Facilitator.
