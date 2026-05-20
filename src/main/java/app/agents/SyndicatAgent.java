412a99e4db52d65454b8c00490cf1fd0ae043c4b
Aaron <161750474+aaidoudi91@users.noreply.github.com>
2026-05-20 11:02:26 +0200

    Màj SyndicatAgent.java

--- src/main/java/app/agents/SyndicatAgent.java
+++ src/main/java/app/agents/SyndicatAgent.java
@@ -3,26 +3,34 @@ package app.agents;
 import app.behaviours.NegotiationBehaviour;
 import app.knowledge.SyndicatKB;
 import app.raison.RaisonClient;
+import app.raison.RaisonOption;
+
 import jade.core.Agent;
 import jade.domain.DFService;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.FIPAException;
+import java.util.List;
 
 /** Agent représentant le Syndicat des travailleurs dont l'objectif est de protéger les emplois et maximiser les
  * garanties pour les travailleurs. Il est récepteur de la négociation. */
 public class SyndicatAgent extends Agent {
     private static final String API_KEY = "";
-    private static final String APP_ID = "PRJ30475"; // Projet RAISON du Syndicat
+    private static final String APP_ID = "PRJ30475"; // Projet rAIson du Syndicat
 
     @Override
     protected void setup() {
         System.out.println("[SYNDICAT] Agent démarré : " + getAID().getName());
         registerToDF(); // S'enregistre pour que la Direction puisse le trouver
-        RaisonClient raisonClient = new RaisonClient(API_KEY, APP_ID);
-        raisonClient.printMetadata();
+        List<RaisonOption> syndicatOptions = List.of(
+                new RaisonOption("OPT411418"), // counter_propose_all
+                new RaisonOption("OPT408968"), // counter_propose_compensation
+                new RaisonOption("OPT408918"), // reject_offer
+                new RaisonOption("OPT408818"), // counter_propose_requalification
+                new RaisonOption("OPT408768")); // accept_offer
+        RaisonClient raisonClient = new RaisonClient(API_KEY, APP_ID, syndicatOptions);
         // Lance le comportement de négociation avec sa propre KB et son instance RAISON
-        // destinataire = null : sera découvert depuis le premier message reçu
+        // Le Syndicat est récepteur : son partenaire sera découvert depuis le premier message reçu (partnerAID = null)
         addBehaviour(new NegotiationBehaviour(this, new SyndicatKB(), null, false, raisonClient));
     }
 
