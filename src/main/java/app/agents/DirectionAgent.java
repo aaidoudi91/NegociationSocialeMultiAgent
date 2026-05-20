a3949099b548c085358374c879ed03586a1c43e0
Aaron <161750474+aaidoudi91@users.noreply.github.com>
2026-05-20 10:59:22 +0200

    Màj DirectionAgent.java

--- src/main/java/app/agents/DirectionAgent.java
+++ src/main/java/app/agents/DirectionAgent.java
@@ -3,6 +3,8 @@ package app.agents;
 import app.behaviours.NegotiationBehaviour;
 import app.knowledge.DirectionKB;
 import app.raison.RaisonClient;
+import app.raison.RaisonOption;
+
 import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.TickerBehaviour;
@@ -10,12 +12,13 @@ import jade.domain.DFService;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.FIPAException;
+import java.util.List;
 
 /** Agent représentant la Direction de l'entreprise, dont l'objectif est de maximiser le ROI tout en minimisant les
- * concessions. Il est initiateur de la négociation. */
+ *  concessions. Il est initiateur de la négociation. */
 public class DirectionAgent extends Agent {
     private static final String API_KEY = "";
-    private static final String APP_ID = "PRJ31675"; // Projet RAISON de la Direction
+    private static final String APP_ID = "PRJ31675"; // Projet rAIson de la Direction
 
     @Override
     protected void setup() {
@@ -28,10 +31,18 @@ public class DirectionAgent extends Agent {
                 AID partnerAID = searchDF();
                 if (partnerAID != null) {
                     System.out.println("[DIRECTION] Syndicat trouvé : " + partnerAID.getLocalName());
-                    RaisonClient raisonClient = new RaisonClient(API_KEY, APP_ID);
-                    raisonClient.printMetadata();
+                    List<RaisonOption> directionOptions = List.of(
+                            new RaisonOption("OPT409318"), // reject_offer
+                            new RaisonOption("OPT409268"), // counter_propose_jobs
+                            new RaisonOption("OPT409218"), // counter_propose_budget
+                            new RaisonOption("OPT409168"), // counter_propose_timeline
+                            new RaisonOption("OPT409118")  // accept_offer
+                    );
+                    RaisonClient raisonClient = new RaisonClient(API_KEY, APP_ID, directionOptions);
+                    // raisonClient.printMetadata();
                     // Initialise la négociation avec sa KB, l'AID du partenaire et son instance RAISON
-                    addBehaviour(new NegotiationBehaviour(myAgent, new DirectionKB(), partnerAID, true, raisonClient));
+                    addBehaviour(new NegotiationBehaviour(myAgent, new DirectionKB(), partnerAID, 
+                            true, raisonClient));
                     stop();
                 } else {
                     System.out.println("[DIRECTION] Syndicat introuvable, nouvelle tentative...");
